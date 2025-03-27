cb727fe2f467eadab3030f1fdedcda329b7ab35f
George Kaceli <georgekaceli8@gmail.com>
2025-03-27 18:52:14 -0400

    updated metrics.py

--- metrics.py
+++ metrics.py
@@ -100,8 +100,7 @@ def compute_word_level_gloss_accuracy(predictions: list, targets: list) -> dict:
 
 def compute_morpheme_level_gloss_accuracy(predictions: list, targets: list) -> dict:
     """
-    Computes word-level glossing accuracy over a set of predictions.
-    This implementation follows the eval_accuracy function from eval.py.
+    Computes morpheme-level glossing accuracy over a set of predictions.
     For each prediction–target pair (both as strings), it splits into tokens,
     compares tokens in order (ignoring any predicted '[UNK]'), and computes:
       - average_accuracy: average of per-example accuracies
